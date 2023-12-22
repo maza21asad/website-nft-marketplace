72dd0074d6ef9e6dbf977e9b7e5eb86deb8ba061
Mohammed Asaduzzaman Asad <maza21asad@gmail.com>
2023-12-22 17:20:51 +0600

    2nd commit

--- src/app/Resume/page.tsx
+++ src/app/Resume/page.tsx
@@ -1,9 +1,12 @@
 import Wrapper from "@/shared/Wrapper";
+import Image from "next/image";
+import ResumeAsad from '@/assets/Resume - Mohammed Asaduzzaman Asad.jpg'
 
 function Resume() {
     return (
         <Wrapper>
-            <div className='top-[30px] relative'>Mohammed Asaduzzaman Asad <br/> Resume</div>
+            <div className='top-[30px] left-[130px] text-[30px] relative'>Mohammed Asaduzzaman Asad <br/> Resume</div>
+            <div className='top-[50px] left-[90px] relative'><Image src={ResumeAsad} alt=""/> </div>
         </Wrapper>
         
     );
